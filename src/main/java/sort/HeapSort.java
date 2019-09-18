b5fb2cedaa0ece1fec91df053e173e5042e3a68e
michaeldeepsearch <Michael-crazyman>
2019-09-18 23:33:37 +0800

    heap sort

---
+++ src/main/java/sort/HeapSort.java
@@ -0,0 +1,64 @@
+package sort;
+
+public class HeapSort {
+
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length <= 0) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);//FIXME why here is always 0
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, (index - 1) / 2, index);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int currentIndex, int size) {
+        int left = currentIndex * 2 + 1;
+
+        while (left < size) {
+            int largestIndex = left + 1 < size && arr[left] > arr[left + 1] ? left : left + 1;
+            largestIndex = arr[largestIndex] > arr[currentIndex] ? largestIndex : currentIndex;
+
+            if (largestIndex == currentIndex) {//FIXME I don't understand why we break
+                break;
+            }
+            swap(arr, largestIndex, currentIndex);
+            currentIndex = largestIndex;
+            left = currentIndex * 2 + 1;
+        }
+
+    }
+
+    public static void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    public static void print(int[] arr) {
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 8, 11, 6, 1, 2, 5};
+
+        heapSort(arr);
+        print(arr);
+    }
+}
